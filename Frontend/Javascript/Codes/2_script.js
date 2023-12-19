8713fc89d7f8bafffba5b942d6bdcb522ebc81d0
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2023-12-20 02:59:29 +0530

    JavaCript Array

---
+++ Frontend/Javascript/Codes/2_script.js
@@ -0,0 +1,72 @@
+// let msg = "Hello ";
+// let londu = "Git hub ";
+// // prompt("Set your password.");
+
+// let password = prompt("This is your password.");
+// let newPass = password.trim();
+// console.log(newPass);
+
+// let monsters = "This is your book.";
+// console.log(monsters.toUpperCase());
+
+// let code = "This is a book";
+// console.log(code);
+// code.toUpperCase();
+// console.log(code.toUpperCase());
+
+
+// let msg="This is good.";
+// //msg.indexOf(0);
+// msg.indexOf("is");
+// msg.indexOf("h");
+// msg.indexOf("p");
+// console.log(msg.indexOf("is"));
+// console.log(msg.indexOf("p"));
+
+
+// trim
+
+let message = "  Hello  ";
+let newmsg = message.trim();
+console.log(newmsg);
+let Up = newmsg;
+console.log(Up.toUpperCase());
+
+
+// slice
+
+let name = "Patra Kirsani";
+console.log(name);
+console.log(name.slice(1, 4));
+console.log(name.slice(-2));
+
+// replace
+
+let Replace = "Karnataka";
+Replace.replace("Kar","tak");
+
+
+// repeat
+
+let name2 = "Discipline";
+
+
+// Practice questions
+
+let msg = "help!";
+console.log(msg.trim());
+
+console.log(msg.toUpperCase());
+
+
+let college = "Consistency";
+// collge.slice().replace('s','t');
+
+
+
+
+
+
+
+
+
---
+++ Frontend/Javascript/Codes/3_app.js
@@ -0,0 +1,8 @@
+let students = ["Patra", "Kirsani", "Moto"];
+let nums = [3,34,343,50];
+console.log(nums);
+
+let info = ["Patra", 23, 3222];
+console.log(info);
+
+let empty = [];
