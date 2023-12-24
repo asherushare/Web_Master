99e6120c8aedbf6b819209d7b4b8a53095fe0e29
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2023-12-24 22:53:57 +0530

    added objects code in JavaScript section

---
+++ Frontend/Javascript/Codes/Objects/app.js
@@ -0,0 +1,148 @@
+// const delhi = {
+//   key: "Patra",
+//   age: 23,
+//   marks: 94.4
+// };
+
+// console.log(delhi)
+
+// const item = {
+//   price: 100,
+//   discount: 50,
+//   colors: ["red", "pink"]
+// };
+
+// console.log(item);
+// console.log(delhi)
+
+// delhi['age']
+
+// const obj = {
+//   1: "a",
+//   2: "b",
+//   true: "c",
+//   null: "d",
+//   undefined: "e"
+// };
+
+// console.log(obj)
+
+// const student = {
+//   name: "Patra",
+//   age: 21,
+//   marks: 77,
+//   city: "Jeypore"
+// };
+
+// console.log(student);
+
+// student.city = "Karnataka";
+// student.gender = "male"
+
+// console.log(student)
+
+
+// delete student.age
+
+// console.log(student)
+
+
+// const classInfo = {
+//   patra: {
+//     age: 21,
+//     class: "1st year",
+//     roll_num: 323
+//   },
+//   Baje: {
+//     grade: "Nothing",
+//     marks: 32
+//   },
+//   Bhimsen: {
+//     village: "Semala",
+//     block: "Nandapur",
+//     ground: "Field"
+//   }
+// }
+
+// console.log(classInfo);
+// console.log(classInfo.patra.class)
+
+// const info = [
+//   {
+//     name: "patra",
+//     grade: "A+"
+//   },
+//   {
+//     name: "kirsani",
+//     age: "34"
+//   }
+// ]
+
+// console.log(info);
+// console.log(Math.PI);
+
+
+// console.log(Math.abs(-2323))
+// console.log(Math.pow(2,3))
+// console.log(Math.floor(2.7332323))
+// console.log(Math.ceil(64.111111))
+// console.log(Math.random())
+// console.log(Math.random())
+
+
+
+
+let step1 = Math.random()
+let step2 = step1 * 10
+
+console.log(step1)
+console.log(step2)
+console.log(Math.floor(step2))
+
+
+// Generate a random number between 1 to 100.
+
+let num1 = Math.random()* 100 + 1
+console.log(Math.floor(num1))
+console.log(Math.floor(num1))
+console.log(Math.floor(num1))
+
+
+
+// Guessing Game
+
+
+// const max = prompt("Enter max number: ");
+// console.log(max);
+
+
+// const randNum = Math.floor(Math.random()* max) + 1;
+// console.log(randNum);
+
+
+// let guess = prompt("Guess the number: ");
+
+// while(true) {
+//   if(guess == "quit") {
+//     console.log("user quit");
+//     break;
+//   }
+
+//   else if(guess == randNum) {
+//     console.log("Congrats! your guess is correct.");
+//     break;
+//   }
+//   else if(guess > randNum) {
+//     guess = prompt("your guess was too large. Please try again")
+//   }
+//   else {
+//     guess = prompt("your guess was too small. Please try again!")
+//   }
+// }
+
+
+
+
+// Assignments
+
+
