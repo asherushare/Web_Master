03cfac6a02434ec130d51282fc4e2dffdc34ac64
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2024-04-19 20:40:26 +0530

    Dom manupulation in JavaScript

---
+++ Frontend/Javascript/Codes/DOM/Second/app.js
@@ -0,0 +1,33 @@
+// console.log(document.querySelector("div a"));
+
+// console.log(document.querySelectorAll("p"));
+
+// let heading = document.querySelector("p");
+// heading.style;
+
+// let ankar = document.querySelectorAll(".box a");
+// for(let i = 0; i < links.length; i++) {
+//   ankar[i].style.color = "green";
+// }
+
+// let h4 = document.querySelector('h4');
+// console.log(h4.parentElement);
+
+
+// document.createElement('p');
+// let newP = document.createElement('p');
+// console.dir(newP);
+
+// newP.innerText = "hi, I am new p";
+
+
+let smallImages = document.getElementsByClassName("oldImg");
+
+for(let i = 0; i < smallImages.length; i++) {
+  console.dir(smallImages[i].src);
+}
+
+console.log("Patra");
+
+
+// getElementByClassName  continues...
