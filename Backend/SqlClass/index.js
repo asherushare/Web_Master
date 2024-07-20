d12a3bd0198a8af1b1f5db280e9a25256279d1df
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2024-07-20 11:09:59 +0530

    Backend practice with real databases

--- Backend/SqlClass/index.js
+++ Backend/SqlClass/index.js
@@ -1,25 +1,66 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require('express');
+const app = express();
 
 const connection = mysql.createConnection ({
-  host: localhost,
+  host: 'localhost',
   user: 'root',
   database: 'new_app',
   password: 'Patra@9090'
 });
 
-connection.query("SHOW TABLES"(err, result) => {
-  if(err) throw err;
-  console.log(result);
-})
+// let q = "show tables";
+
+// try {
+//   connection.query(q, (err, result) => {
+//   if(err) throw err;
+//   console.log(result[0]);
+//   console.log(result[1]);
+//   })
+// } catch(err) {
+//   console.log(err);
+// }
 
 let createRandomUser = () => {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
+  return [
+    faker.string.uuid(),
+    faker.internet.userName(),
+    faker.internet.email(),
+    faker.internet.password(),
+];
 }
 
-console.log(createRandomUser());
+
+app.get("/", (req, res) => {
+  let q = "select count(*) from user";
+  try {
+  connection.query(q, (err, result) => {
+  if(err) throw err;
+  console.log(result[0]["count(*)"]);
+  res.send("success");
+  })
+
+} catch(err) {
+  console.log(err);
+  res.send("some error in database");
+}
+})
+
+app.listen("8080", () => {
+  console.log("server is listening to port 8080");
+});
+
+
+
+//try {
+//   connection.query(q, [data], (err, result) => {
+//   if(err) throw err;
+//   console.log(result[0]);
+//   console.log(result[1]);
+//   })
+// } catch(err) {
+//   console.log(err);
+// }
+
+// connection.end();
