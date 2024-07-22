df503e662603cafab8015580caa36c1a4c922ca3
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2024-07-22 10:18:55 +0530

    New commit into sqlclass

--- Backend/SqlClass/index.js
+++ Backend/SqlClass/index.js
@@ -2,6 +2,10 @@ const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, '/views'));
 
 const connection = mysql.createConnection ({
   host: 'localhost',
@@ -31,20 +35,39 @@ let createRandomUser = () => {
 ];
 }
 
+// Home route
 
 app.get("/", (req, res) => {
   let q = "select count(*) from user";
   try {
   connection.query(q, (err, result) => {
   if(err) throw err;
-  console.log(result[0]["count(*)"]);
-  res.send("success");
+  let count = result[0]["count(*)"];
+  res.render("home.ejs", { count });
   })
 
 } catch(err) {
   console.log(err);
   res.send("some error in database");
 }
+})
+
+// Show Route
+
+app.get("/user", (req, res) => {
+  res.send("success");
+  let q = "select * from user";
+
+  try {
+    connection.query(q, (err, result) => {
+      if(err) throw err;
+      // console.log(result);
+      res.render("showusers.ejs");
+    });
+  } catch(err) {
+    console.log(err);
+    res.send("some error in database");
+  }
 })
 
 app.listen("8080", () => {
