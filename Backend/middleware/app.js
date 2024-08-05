e2b2b6c8079d1a89a22e293db60b7cf517a65361
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2024-08-05 08:05:52 +0530

    middleware creating

--- Backend/middleware/app.js
+++ Backend/middleware/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("./ExpressError");
 
 // app.use((req, res, next) => {
 //   console.log("Hi, I am 1st middleware");
@@ -11,9 +12,37 @@ const app = express();
 //   next();
 // })
 
-app.use((req, res, next) => {
-  console.log(req.method, req.hostname, req.pathname, req.path);
-  next();
+// app.use((req, res, next) => {
+//   console.log(req.method, req.hostname, req.pathname, req.path);
+//   next();
+// })
+
+// app.use("/api", (req, res, next) => {
+//   let {token} = req.query;
+//   if(token === "giveaccess") {
+//     next();
+//   }
+//   res.send("ACCESS DENIED");
+// })
+
+// app.get("/api", (req, res) => {
+//   res.send("data");
+// })
+
+
+// we can pass middleware as function
+
+
+const checkToken = ("/api", (req, res, next) => {
+  let {token} = req.query;
+  if(token === "giveaccess") {
+    next();
+  }
+  throw new ExpressError(401, "ACCESS DENIED");
+})
+
+app.get("/api", checkToken, (req, res) => {
+  res.send("data");
 })
 
 app.get("/", (req, res) => {
@@ -23,6 +52,21 @@ app.get("/", (req, res) => {
 app.get("/random", (req, res) => {
   res.send("this is random page!");
 })
+
+app.get("/err", (req, res) => {
+  abcd = abcd;
+})
+
+app.use((err, req, res, next) => {
+  console.log("-----ERRPR----");
+  res.send(err);
+})
+
+// app.use((err, req, res, next) => {
+//   console.log("-----ERRPR 2----");
+//   next(err);
+// })
+
 
 app.listen(8080, () => {
   console.log("listening on post 8080");
