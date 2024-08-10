0210480816a62345a86aa8dc08e4b2bde0f1349e
name <asherushare@gmail.com>
2024-08-10 11:28:05 +0530

    React 2nd day

---
+++ react/basic-react-app/src/ProductTab.jsx
@@ -0,0 +1,14 @@
+import Product from "./product.jsx";
+
+
+function ProductTab() {
+  return (
+    <>
+    <Product />
+    <Product />
+    <Product />
+    </>
+  )
+}
+
+export default ProductTab;
